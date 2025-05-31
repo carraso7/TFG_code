d9236461faf649a66ae8c86e9ac290dbb081275d
carraso7 <carlosrasoalonso@gmail.com>
2025-06-01 01:04:47 +0200

    First implementation of http://graphdrawing.org/data.html tests

--- sequential_implementation/paralel_planarity_criterion/SPQR.py
+++ sequential_implementation/paralel_planarity_criterion/SPQR.py
@@ -10,7 +10,98 @@ from itertools import combinations
 
 
 @staticmethod
-def get_SPQR_tree(G, TCCs):
+def get_SPQR_tree(G, TCCs, sep_pairs):  ### TODO REVISAR DOC Y VER QUÉ HACER CON LOS NODOS QUE NO ESTÁN EN NINGUNA COMPONENTE TRICONECTADA.
+    """
+    Constructs an SPQR-like tree with:
+    - Edges between separation pairs (real if in G, virtual otherwise).
+    - Edges within TCCs that involve uncategorized nodes.
+      (real if in G, virtual otherwise)
+
+    Parameters
+    ----------
+    G : networkx.Graph
+        The original graph.
+    TCCs : list of dicts
+        Triconnected components with 'node_list' and 'virtual_edges'.
+    sep_pairs : list of tuples
+        List of separation pairs.
+
+    Returns
+    -------
+    edges_dict : dict
+        {
+            'real_edges': list of edges present in G,
+            'virtual_edges': list of edges not in G
+        }
+    SPQR_tree : networkx.Graph
+        Graph composed of those edges.
+    """
+    import networkx as nx
+
+    # Get nodes categorized in any of the triconnected components.
+    categorized_nodes = set()
+    # print(TCCs) ### TODO PRINT QUITAR
+    for tcc in TCCs:
+        # print(tcc) ### TODO PRINT QUITAR
+        categorized_nodes.update(tcc["node_list"])
+    # print(categorized_nodes) ### TODO PRINT QUITAR
+    
+    
+    all_nodes = set(G.nodes())
+    uncategorized_nodes = all_nodes - categorized_nodes
+    # print("uncategorized: ", uncategorized_nodes) ### TODO PRINT QUITAR
+    real_edges = set()
+    virtual_edges = set()
+
+    # Process separation pairs
+    for u, v in sep_pairs:
+        edge = tuple(sorted((u, v)))
+        if G.has_edge(*edge):
+            real_edges.add(edge)
+        else:
+            virtual_edges.add(edge)
+    
+    for e in G.edges():
+        if (e[0] in uncategorized_nodes) or (e[1] in uncategorized_nodes):
+            real_edges.add(e)
+        
+    
+    # # Process edges inside TCCs involving uncategorized nodes
+    # for tcc in TCCs:
+    #     tcc_nodes = set(tcc["node_list"])
+    #     tcc_uncategorized = tcc_nodes & uncategorized_nodes
+    #     print(tcc_uncategorized)
+    #     if tcc_uncategorized:
+    #         # Add internal real edges
+    #         for u in tcc_nodes:
+    #             for v in tcc_nodes:
+    #                 if u < v and ((u in tcc_uncategorized) or (v in tcc_uncategorized)):
+    #                     edge = tuple(sorted((u, v)))
+    #                     if G.has_edge(*edge):
+    #                         real_edges.add(edge)
+
+    #         # Add virtual edges that involve any uncategorized node and are not real
+    #         for u, v in tcc["virtual_edges"]:
+    #             edge = tuple(sorted((u, v)))
+    #             if (u in tcc_uncategorized or v in tcc_uncategorized) and not G.has_edge(*edge):
+    #                 virtual_edges.add(edge)
+
+    # Build SPQR tree
+    SPQR_tree = nx.Graph()
+    SPQR_tree.add_edges_from(real_edges)
+    SPQR_tree.add_edges_from(virtual_edges)
+
+    edges_dict = {
+        "real_edges": list(real_edges),
+        "virtual_edges": list(virtual_edges),
+    }
+
+    return edges_dict, SPQR_tree
+
+
+
+@staticmethod
+def get_SPQR_tree2(G, TCCs):
     """
     Constructs an SPQR-like tree that includes:
     - all virtual edges from TCCs,
